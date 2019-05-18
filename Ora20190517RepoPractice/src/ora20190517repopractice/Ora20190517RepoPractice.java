c155f7eadf6a07f8279ffc4134a83cc2dd940268
Prynerd <atti13@gmail.hu>
2019-05-18 14:36:22 +0200

    add printPeopleInCity method

--- Ora20190517RepoPractice/src/ora20190517repopractice/Ora20190517RepoPractice.java
+++ Ora20190517RepoPractice/src/ora20190517repopractice/Ora20190517RepoPractice.java
@@ -6,7 +6,10 @@
 package ora20190517repopractice;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -17,8 +20,25 @@ public class Ora20190517RepoPractice {
     public static void main(String[] args) throws FileNotFoundException {
 
         Reader.read();
-        Collections.sort(Reader.persons,new PersonNameComparator());
+        Collections.sort(Reader.persons, new PersonNameComparator());
     }
-    
+
+    public static void printPeopleInCity() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Adjon meg egy várost: ");
+        String city = sc.nextLine();
+        List<Person> cloneList = new ArrayList<>();
+        for (Person person : Reader.persons) {
+            cloneList.add(person);
+        }
+        List<Person> peopleInCity = Filter.cityFilter(city, cloneList);
+        if (peopleInCity.size() > 0) {
+            for (Person person : peopleInCity) {
+                System.out.println(person.getName());
+            }
+        } else {
+            System.out.println("Ebben a városban lakó emberről nincs adatunk.");
+        }
+    }
 
 }
